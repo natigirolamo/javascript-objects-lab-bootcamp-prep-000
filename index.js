7b901a466e3696e6b850b982eb36f356a348b64f
exuberant-glitch-6816 <natalia.girolamo@gmail.com>
2019-05-12 14:16:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,22 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
   object [key] = value
   return object 
 }
+function deleteFromObjectByKey
+ describe('deleteFromObjectByKey(object, key)', function() {
+    it('deletes `key` from a clone of object and returns the new object (it is non-destructive)', function() {
+      var obj = { prop: 1 }
+      var newObj = deleteFromObjectByKey(obj, 'prop')
 
+      expect(newObj['prop']).toBe(undefined)
+    })
+
+    it('does not modify the original object (it is non-destructive)', function() {
+      var obj = { prop: 1 }
+
+      deleteFromObjectByKey(obj, 'prop')
+      expect(obj['prop']).toBe(1)
+    })
+  })
 
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
