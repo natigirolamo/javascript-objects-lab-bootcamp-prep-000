8d0f5bc6ba81c8dab9131426184a392da9af0f45
exuberant-glitch-6816 <natalia.girolamo@gmail.com>
2019-05-12 14:07:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,27 +4,6 @@ function updateObjectWithKeyAndValue(object, key, value){
 return Object.assign({}, object, {[key]: value});
 }
 
-function updateObjectWithKeyAndValue(object,key,value){ 
-object [key] = value;
-return object
-}
-
-  describe('destructivelyUpdateObjectWithKeyAndValue(object, key, value)', function() {
-    it('updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object', function() {
-      var obj = { prop: 1 }
-
-      expect(destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
-        prop: 1,
-        prop2: 2
-      })
-
-      expect(obj).toMatch({
-        prop: 1,
-        prop2: 2
-      })
-    })
-  })
-
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
   object [key] = value
   return object 
