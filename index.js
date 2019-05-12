7f034e33dc27109614e65c36084a99b7abd13d28
exuberant-glitch-6816 <natalia.girolamo@gmail.com>
2019-05-12 14:21:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var recipes {};
 function updateObjectWithKeyAndValue(object, key, value){
 return Object.assign({}, object, {[key]: value});
@@ -14,7 +13,21 @@ delete newobject[key];
 return newObjt;
 }
 
+function destructivelyDeleteFromObjectByKey(object,key)
+var 
+describe('destructivelyDeleteFromObjectByKey(object, key)', function() {
+    it('returns object without the delete key/value pair', function() {
+      var obj = { prop: 1 }
+      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
 
+      expect(newObj['prop']).toBe(undefined)
+    })
+
+    it('modifies the original object', function() {
+      var obj = { prop: 1 }
+      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
+
+      expect(obj['prop']).toBe(undefined)
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
