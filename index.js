514db1f71e34cd869087ea1a84559f30094d1e57
exuberant-glitch-6816 <natalia.girolamo@gmail.com>
2019-05-12 14:25:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
   object [key] = value
   return object 
 }
-function deleteFromObjectByKey(object,key){
+function deleteFromObjectByKey(object,key) {
 var newObj = Object.assing({}), object);
 delete newObj[key];
 return newObj;
